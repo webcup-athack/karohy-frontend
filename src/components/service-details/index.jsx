07d6c9e451739c728428501ab105f303d068bbe9
Tahiana Andriambahoaka <89937368+TahianaAndriambahoaka@users.noreply.github.com>
2023-06-24 16:01:00 +0300

    Update index.jsx

--- src/components/service-details/index.jsx
+++ src/components/service-details/index.jsx
@@ -1,26 +1,27 @@
-import React, { useEffect } from 'react';
-import { FooterThree, HeaderSix, Wrapper } from '../../layout';
-import { animationCreate } from '../../utils/utils';
-import Breadcrumb from '../common/breadcrumb/breadcrumb';
-import AccordionArea from './accordion-area';
-import ServiceDetailsArea from './service-details-area';
+import React, { useEffect } from "react";
+import { FooterThree, Header, HeaderSix, Wrapper } from "../../layout";
+import { animationCreate } from "../../utils/utils";
+import Breadcrumb from "../common/breadcrumb/breadcrumb";
+import AccordionArea from "./accordion-area";
+import ServiceDetailsArea from "./service-details-area";
+import Breadcrumb2 from "../common/breadcrumb/breadcrumb2";
 
 const ServiceDetails = ({ service }) => {
-	useEffect(() => {
-		setTimeout(() => {
-			animationCreate();
-		}, 500);
-	}, []);
+  useEffect(() => {
+    setTimeout(() => {
+      animationCreate();
+    }, 500);
+  }, []);
 
-	return (
-		<Wrapper>
-			<HeaderSix />
-			<Breadcrumb service={service} />
-			<ServiceDetailsArea service={service} />
-			{/* <AccordionArea /> */}
-			<FooterThree />
-		</Wrapper>
-	);
+  return (
+    <Wrapper>
+      <Header />
+      <Breadcrumb2 service={service} />
+      <ServiceDetailsArea service={service} />
+      {/* <AccordionArea /> */}
+      <FooterThree />
+    </Wrapper>
+  );
 };
 
 export default ServiceDetails;
