234d523d3f6b45ad53a007a45d1e44972da3cadf
Tahiana Andriambahoaka <89937368+TahianaAndriambahoaka@users.noreply.github.com>
2023-06-24 17:07:00 +0300

    services similaires - details services

--- src/components/service-details/service-details-area.jsx
+++ src/components/service-details/service-details-area.jsx
@@ -1,7 +1,26 @@
 import React from "react";
 import { styled } from "styled-components";
+import { services_data } from "../../data";
+import SingleServiceTwo from "../common/single-service-2";
+
+const getRandomIndexes = (n, size) => {
+  const numbers = [];
+  for (let i = 0; i < size; i++) {
+    numbers.push(i);
+  }
+  for (let i = numbers.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [numbers[i], numbers[j]] = [numbers[j], numbers[i]];
+  }
+  return numbers.slice(0, n);
+};
 
 const ServiceDetailsArea = ({ service }) => {
+  const randomIndexes = getRandomIndexes(2, services_data.length);
+  const randomServices = [];
+  for (let i = 0; i < randomIndexes.length; i++) {
+    randomServices.push(services_data[randomIndexes[i]]);
+  }
   return (
     <>
       <div className="service-details-area">
@@ -98,17 +117,17 @@ const ServiceDetailsArea = ({ service }) => {
                       Gallerie
                     </H3>
                     <div
-                      className="tp-hero-text"
+                      className="tp-hero-text mt-10"
                       style={{
                         height: 5,
                         backgroundColor: "#ED254E",
-                        width: "200%",
+                        width: "80%",
                         marginLeft: "50%",
                         transform: "translate(-50%, 0%)",
                       }}
                     />
                   </div>
-                  <div className="mt-100 d-flex align-items-center justify-content-center">
+                  <div className="mt-80 d-flex align-items-center justify-content-center">
                     <div
                       style={{ position: "relative", width: 350, height: 350 }}
                     >
@@ -142,30 +161,47 @@ const ServiceDetailsArea = ({ service }) => {
                   </div>
                 </div>
               </div>
-              <div className="row mt-100 text-center">
+              <div className="row mt-100">
                 <div className="col">
-                  <div
-                    className="tp-hero-text"
-                    style={{
-                      display: "inline-block",
-                    }}
-                  >
-                    <H3
-                      className="service-details-space"
-                      style={{ fontSize: 35 }}
-                    >
-                      Services similaires
-                    </H3>
+                  <div className=" text-center d-flex justify-content-center">
                     <div
                       className="tp-hero-text"
                       style={{
-                        height: 5,
-                        backgroundColor: "#ED254E",
-                        width: "120%",
-                        marginLeft: "50%",
-                        transform: "translate(-50%, 0%)",
+                        display: "inline-block",
                       }}
-                    />
+                    >
+                      <H3
+                        className="service-details-space"
+                        style={{ fontSize: 35 }}
+                      >
+                        Services similaires
+                      </H3>
+                      <div
+                        className="tp-hero-text mt-10"
+                        style={{
+                          height: 5,
+                          backgroundColor: "#ED254E",
+                          width: "70%",
+                          marginLeft: "50%",
+                          transform: "translate(-50%, 0%)",
+                        }}
+                      />
+                    </div>
+                  </div>
+                  <div className="row pb-50 mt-80">
+                    <div className="col-md-1 col-lg-1"></div>
+                    <div className="col-md-10 col-lg-10">
+                      <div className="row d-flex justify-content-center align-items-center gap-3">
+                        {randomServices.map((s, i) => (
+                          <SingleServiceTwo
+                            key={i}
+                            service={s}
+                            border={"sv-2-border"}
+                          />
+                        ))}
+                      </div>
+                    </div>
+                    <div className="col-md-1 col-lg-1"></div>
                   </div>
                 </div>
               </div>
