db2e4332d4c304aac51ff55d178453acb59d79fe
Tahiana Andriambahoaka <89937368+TahianaAndriambahoaka@users.noreply.github.com>
2023-07-05 19:09:13 +0300

    call temporaire de ChatGPT API

--- src/components/custom/form/search-form.jsx
+++ src/components/custom/form/search-form.jsx
@@ -1,129 +1,164 @@
-import { useFormik } from 'formik';
-import React, { useState, useRef } from 'react';
-import ReactLoading from 'react-loading';
-import styled from 'styled-components';
-import { toast } from 'react-toastify';
-import { contactSchema } from '../../../utils/validation-schema';
-import ServiceCard from '../../common/service-card';
+import { useFormik } from "formik";
+import React, { useState, useRef } from "react";
+import ReactLoading from "react-loading";
+import styled from "styled-components";
+import { toast } from "react-toastify";
+import { contactSchema } from "../../../utils/validation-schema";
+import ServiceCard from "../../common/service-card";
 
-import { services_data } from '../../../data';
-import SendIcon from '../../../imports/core/ui/SendIcon';
-import ErrorMsg from './error-msg';
+import { services_data } from "../../../data";
+import SendIcon from "../../../imports/core/ui/SendIcon";
+import ErrorMsg from "./error-msg";
 
 function getRandomIndexes(n, size) {
-	const numbers = [];
-	for (let i = 0; i < size; i++) {
-		numbers.push(i);
-	}
-	for (let i = numbers.length - 1; i > 0; i--) {
-		const j = Math.floor(Math.random() * (i + 1));
-		[numbers[i], numbers[j]] = [numbers[j], numbers[i]];
-	}
-	return numbers.slice(0, n);
+  const numbers = [];
+  for (let i = 0; i < size; i++) {
+    numbers.push(i);
+  }
+  for (let i = numbers.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [numbers[i], numbers[j]] = [numbers[j], numbers[i]];
+  }
+  return numbers.slice(0, n);
 }
 function getRandomNumberInRange(a, b) {
-	const min = Math.min(a, b);
-	const max = Math.max(a, b);
-	return Math.floor(Math.random() * (max - min + 1)) + min;
+  const min = Math.min(a, b);
+  const max = Math.max(a, b);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 const SearchForm = ({ stateSearch, setStateSearch }) => {
-	// user
-	// const { user } = useSelector(state => state.auth);
-	// console.log(user)
-	const [searchResult, setSearchResult] = useState([]);
-	const [loading, setLoading] = useState(false);
-	const buttonRef = useRef(null);
-	const { handleChange, handleSubmit, handleBlur, errors, values, touched } =
-		useFormik({
-			initialValues: { name: '', email: '', msg: '' },
-			validationSchema: contactSchema,
-			onSubmit: (values, { resetForm }) => {
-				console.log('on submit');
-				toast.success(`${values.name} your message sent successfully`, {
-					position: 'top-left',
-				});
-				resetForm();
-			},
-		});
+  // user
+  // const { user } = useSelector(state => state.auth);
+  // console.log(user)
+  const [searchResult, setSearchResult] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const buttonRef = useRef(null);
+  const { handleChange, handleSubmit, handleBlur, errors, values, touched } =
+    useFormik({
+      initialValues: { name: "", email: "", msg: "" },
+      validationSchema: contactSchema,
+      onSubmit: (values, { resetForm }) => {
+        console.log("on submit");
+        toast.success(`${values.name} your message sent successfully`, {
+          position: "top-left",
+        });
+        resetForm();
+      },
+    });
 
-	const getSearchResult = (number) => {
-		buttonRef.current.scrollIntoView({ behavior: 'smooth' });
-		setLoading(true);
-		const randomIndexes = getRandomIndexes(number, services_data.length);
-		const randomServices = [];
-		for (let i = 0; i < randomIndexes.length; i++) {
-			randomServices.push(services_data[randomIndexes[i]]);
-		}
-		setSearchResult(randomServices);
-		setTimeout(() => {
-			setLoading(false);
-			if (buttonRef.current) {
-				buttonRef.current.scrollIntoView({
-					behavior: 'smooth',
-					block: 'start',
-					inline: 'nearest',
-				});
-			}
-		}, 2000);
-	};
+  const getSearchResult = (number) => {
+    buttonRef.current.scrollIntoView({ behavior: "smooth" });
+    // setLoading(true);
+    const randomIndexes = getRandomIndexes(number, services_data.length);
+    const randomServices = [];
+    for (let i = 0; i < randomIndexes.length; i++) {
+      randomServices.push(services_data[randomIndexes[i]]);
+    }
+    setSearchResult(randomServices);
+    setTimeout(() => {
+      // setLoading(false);
+      if (buttonRef.current) {
+        buttonRef.current.scrollIntoView({
+          behavior: "smooth",
+          block: "start",
+          inline: "nearest",
+        });
+      }
+    }, 2000);
+  };
 
-	const handleSearchSubmit = (e) => {
-		handleSubmit(e);
-		// if (!errors?.msg && values?.msg) {
-		// 	getSearchResult(3);
-		// }
-		getSearchResult(getRandomNumberInRange(2, 5));
-		setStateSearch(true);
-	};
+  const getSousCategoriesCorrespondantes = async () => {
+    setLoading(true);
+    const token = "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";
+    const apiUrl = "https://api.openai.com/v1/chat/completions";
+    const data = {
+      model: "gpt-3.5-turbo",
+      messages: [
+        {
+          role: "user",
+          content:
+            "Avec les catégories suivantes (garagiste,maçon,décorateur,restaurant), lesquelles d'entre elles correspondent avec ce texte (j'ai faim)? Écris juste dans une liste les catégories correspondantes séparés juste par des virgules sans espaces entre elles en respectant la sensibilité à la casse.",
+        },
+      ],
+    };
 
-	return (
-		<>
-			<div className="col-xl-8 col-lg-8" style={{ margin: 'auto' }}>
-				<form id="contact-form" onSubmit={handleSearchSubmit}>
-					<InputGroup>
-						<InputWrapper>
-							<StyledInput
-								value={values.msg}
-								onChange={handleChange}
-								onBlur={handleBlur}
-								name="msg"
-								placeholder="Que recherchez-vous ?"
-							/>
-						</InputWrapper>
-						{/* {touched.msg && <ErrorMsg error={errors.msg} />} */}
-						<IconButton type="submit" ref={buttonRef}>
-							{loading ? (
-								<ReactLoading type="bars" color="red" />
-							) : (
-								<SendIcon />
-							)}
-						</IconButton>
-					</InputGroup>
-				</form>
-				<StyledP
-					className={`${stateSearch && 'show'}`}
-					style={{ margin: '24px 0 0', padding: 0, width: '80%' }}
-				>
-					<small>
-						Karohy, l{"'"}espace numérique dynamique où vous pouvez
-						instantanément dénicher le prestataire de service idéal qui donnera
-						vie à vos projets avec brio!
-					</small>
-				</StyledP>
-			</div>
-			{searchResult.length > 0 && (
-				<ResultWrapper>
-					{!loading && (
-						<>
-							<StyledH1>Résultats</StyledH1>
-							<ResultFlex>
-								{searchResult.map((elem, i) => (
-									<ServiceCard key={i} service={elem} border={'sv-2-border'} />
-								))}
-							</ResultFlex>
-						</>
-					)}
-					{/* {loading ? (
+    try {
+      const response = await fetch(apiUrl, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+        body: JSON.stringify(data),
+      });
+
+      const result = await response.json();
+      console.log(result.choices[0].message.content);
+    } catch (error) {
+      console.error("Error:", error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const handleSearchSubmit = async (e) => {
+    handleSubmit(e);
+    // if (!errors?.msg && values?.msg) {
+    // 	getSearchResult(3);
+    // }
+    await getSousCategoriesCorrespondantes();
+    getSearchResult(getRandomNumberInRange(2, 5));
+    setStateSearch(true);
+  };
+
+  return (
+    <>
+      <div className="col-xl-8 col-lg-8" style={{ margin: "auto" }}>
+        <form id="contact-form" onSubmit={handleSearchSubmit}>
+          <InputGroup>
+            <InputWrapper>
+              <StyledInput
+                value={values.msg}
+                onChange={handleChange}
+                onBlur={handleBlur}
+                name="msg"
+                placeholder="Que recherchez-vous ?"
+              />
+            </InputWrapper>
+            {/* {touched.msg && <ErrorMsg error={errors.msg} />} */}
+            <IconButton type="submit" ref={buttonRef}>
+              {loading ? (
+                <ReactLoading type="bars" color="red" />
+              ) : (
+                <SendIcon />
+              )}
+            </IconButton>
+          </InputGroup>
+        </form>
+        <StyledP
+          className={`${stateSearch && "show"}`}
+          style={{ margin: "24px 0 0", padding: 0, width: "80%" }}
+        >
+          <small>
+            Karohy, l{"'"}espace numérique dynamique où vous pouvez
+            instantanément dénicher le prestataire de service idéal qui donnera
+            vie à vos projets avec brio!
+          </small>
+        </StyledP>
+      </div>
+      {searchResult.length > 0 && (
+        <ResultWrapper>
+          {!loading && (
+            <>
+              <StyledH1>Résultats</StyledH1>
+              <ResultFlex>
+                {searchResult.map((elem, i) => (
+                  <ServiceCard key={i} service={elem} border={"sv-2-border"} />
+                ))}
+              </ResultFlex>
+            </>
+          )}
+          {/* {loading ? (
 						<LoaderWrapper>
 							<ReactLoading type="spinningBubbles" color="#0bbfdc" />
 						</LoaderWrapper>
@@ -137,67 +172,67 @@ const SearchForm = ({ stateSearch, setStateSearch }) => {
 							</ResultFlex>
 						</>
 					)} */}
-				</ResultWrapper>
-			)}
-		</>
-	);
+        </ResultWrapper>
+      )}
+    </>
+  );
 };
 
 const StyledP = styled.p`
-	opacity: 0;
-	transition: opacity 2.5s ease;
-	&.show {
-		opacity: 1;
-	}
+  opacity: 0;
+  transition: opacity 2.5s ease;
+  &.show {
+    opacity: 1;
+  }
 `;
 const IconButton = styled.button``;
 const InputGroup = styled.div`
-	display: flex;
-	flex-direction: row;
-	align-items: center;
-	gap: 10px;
-	@media (max-width: 496px) {
-		flex-direction: column;
-	}
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  gap: 10px;
+  @media (max-width: 496px) {
+    flex-direction: column;
+  }
 `;
 const InputWrapper = styled.div`
-	width: 60%;
-	max-height: 70px;
-	border-radius: 15px;
-	border-bottom: 4px solid #ed254e;
-	background: #fff;
-	box-shadow: 0px 0px 20px 0px rgba(0, 0, 0, 0.25);
-	@media (max-width: 496px) {
-		width: 100%;
-	}
+  width: 60%;
+  max-height: 70px;
+  border-radius: 15px;
+  border-bottom: 4px solid #ed254e;
+  background: #fff;
+  box-shadow: 0px 0px 20px 0px rgba(0, 0, 0, 0.25);
+  @media (max-width: 496px) {
+    width: 100%;
+  }
 `;
 const StyledInput = styled.input`
-	flex-shrink: 0;
-	height: 70px;
+  flex-shrink: 0;
+  height: 70px;
 `;
 const ResultWrapper = styled.div`
-	margin-top: 50px;
+  margin-top: 50px;
 `;
 const ResultFlex = styled.div`
-	width: 100%;
-	display: flex;
-	flex-direction: row;
-	justify-content: space-between;
-	// align-items: stretch;
-	flex-wrap: wrap;
-	gap: 20px;
-	padding: 50px 0;
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  // align-items: stretch;
+  flex-wrap: wrap;
+  gap: 20px;
+  padding: 50px 0;
 `;
 const LoaderWrapper = styled.div`
-	display: flex;
-	justify-content: center;
-	padding-top: 75px;
+  display: flex;
+  justify-content: center;
+  padding-top: 75px;
 `;
 const StyledH1 = styled.h1`
-	margin: 20px 0;
-	padding: 0 0 10px;
-	border-bottom: 3px #ed254e solid;
-	// width: 100%;
-	display: inline-block;
+  margin: 20px 0;
+  padding: 0 0 10px;
+  border-bottom: 3px #ed254e solid;
+  // width: 100%;
+  display: inline-block;
 `;
 export default SearchForm;
