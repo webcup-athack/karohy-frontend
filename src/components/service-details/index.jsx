147918b5a589ab84ee8eee7fbb01514c6357065d
AnthonyDurandal <a.durandal@outlook.com>
2023-08-30 19:16:29 +0300

    refactor:remove accordion component

--- src/components/service-details/index.jsx
+++ src/components/service-details/index.jsx
@@ -1,8 +1,6 @@
 import React, { useEffect } from "react";
 import { Header, Wrapper } from "../../layout";
 import { animationCreate } from "../../utils/utils";
-import Breadcrumb from "../common/breadcrumb/breadcrumb";
-import AccordionArea from "./accordion-area";
 import ServiceDetailsArea from "./service-details-area";
 import Breadcrumb2 from "../common/breadcrumb/breadcrumb2";
 
@@ -18,7 +16,6 @@ const ServiceDetails = ({ service }) => {
       <Header />
       <Breadcrumb2 service={service} />
       <ServiceDetailsArea service={service} />
-      {/* <AccordionArea /> */}
     </Wrapper>
   );
 };
